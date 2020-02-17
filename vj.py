a1806d2aed625eb57845b747aaee06ee2261e60b
vijay <61132583+vijayanand55@users.noreply.github.com>
2020-02-17 12:35:08 +0530

    Add files via upload

---
+++ vj.py
@@ -0,0 +1,11 @@
+import random
+r=input("Do you Want to Play the Game:")
+while(1):
+    if(r=="Yes" or r=="YES" or r=="Yes"):
+        y=random.randint(1,6)
+        print(y)
+    elif(r=="NO" or r=="No" or r=="No"):
+        print("Thanks for Playing the Game")
+        break
+    
+
